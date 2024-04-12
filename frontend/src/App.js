1ec57ce146cca4a425684ff03e3799bfca6ffc5c
Jaya Mishra <mjaya7519@gmail.com>
2024-04-12 13:00:52 +0530

    "comm"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar from "./components/Navbar/Navbar.js";
+import Home from "./components/Home/Home.js";
+import Register from "./components/Register/Register.js";
+import Edit from "./components/Edit/Edit.js";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./components/Detail/Detail.js";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/view/:id" element={<Detail />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Context from "./components/Context/Context";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Context>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Context>
 );
